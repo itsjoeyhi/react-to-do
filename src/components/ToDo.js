a9fc142db8b2b0101b5f4fb7cb2a32c459dd1e32
Joseph <joseph@Josephs-MacBook-Pro.local>
2018-11-19 20:39:46 -0500

    checkpoint done w q :w q

---
+++ src/components/ToDo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+
+class ToDo extends Component {
+  render() {
+    return (
+        <div className='App'>
+      <li> A todo will go here </li>
+    </div>
+    );
+  }
+}
+
+export default ToDo;
